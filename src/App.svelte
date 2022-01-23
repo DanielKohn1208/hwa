4006b05b4f7f896698e34254fefe7d3ec9adebae
DanielKohn1208 <danielmkohn1208@gmail.com>
2022-01-22 19:21:33 -0500

    got sound to work

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,11 @@
 <script>
 	import Timer from './Timer.svelte';
 	import HowTo from './HowTo.svelte';
+	let audio;
+	function timerEnds(){
+		audio.play();
+		 
+	}
 </script>
 <style>
 	h1,h3{
@@ -9,10 +14,14 @@
 </style>
 <h1>Handwashing App</h1>
 
-<Timer />
+<Timer on:end={timerEnds}/>
 
 <HowTo />
 <h3>
 	<a href="https://www.who.int/teams/integrated-health-services/infection-prevention-control">Picture source</a>
 	<a href="https://freesound.org/people/metrostock99/sounds/345086/">Sound Effect</a>
 </h3>
+
+<audio bind:this={audio}>
+	<source src="public_sound.wav" />
+</audio>
