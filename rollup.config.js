5c9733e6a5b8182e821a3472294e4158b08a5cff
DanielKohn1208 <danielmkohn1208@gmail.com>
2022-01-22 19:36:46 -0500

    fixing github issues

--- rollup.config.js
+++ rollup.config.js
@@ -69,7 +69,7 @@ export default {
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
-		production && terser() && ghPages.publish('public',() =>{
+		production && terser() && ghPages.publish('public',(err) =>{
 			console.log('published to github',err);
 
 		})
